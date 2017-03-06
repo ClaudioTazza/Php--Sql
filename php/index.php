b1938ee1de908ae6faae1296fa68fb78534b6115
lucifero55 <claudiotazza@gmail.com>
2017-03-06 13:03:20 +0100

    Modifiche

--- php/index.php
+++ php/index.php
@@ -2,7 +2,7 @@
 Realizzare una applicazione che tramite la scelta dei prodotti e quantità dei prodotti mi generi la somma da pagare.
 Sapendo che se la somma da pagare è superiore di 100 euro la spedizione è gratuita,
 se si possiede una carta fedeltà c'è lo sconto del 10%.
-La spedizione costa 10 euro, gli sconti si sovrappongono.
+La spedizione costa 10%, gli sconti si sovrappongono.
 -->
 <!doctype html>
 
@@ -14,15 +14,24 @@ La spedizione costa 10 euro, gli sconti si sovrappongono.
 
   <body>
 
-  <form action="index.php" method="POST">
-    <p>Nike 80$
-    <input type="checkbox" name="Nike" value=80></p><br>
+  <h3>Warning</h3>
+  <p>La spedizione si paga 50€.</p>
+  <p>I possessori della carta federltà hanno diritto al 10% di sconto.</p>
+  <p>Per acquisti superiori a 100€ la spedizione è gratuita.</p><br>
 
-    <p>Hogan 20$
-    <input type="checkbox" name="Hogan" value=20></p><br>
+  <form action="index.php" method="POST">
+    <p>Nike 80€
+    <input type="checkbox" name="Nike" value=80>
+    Quantità:
+    <input type="number" name="NNike" min=0></p><br>
+
+    <p>Hogan 20€
+    <input type="checkbox" name="Hogan" value=20>
+    Quantità:
+    <input type="number" name="NHogan" min=0></p><br>
 
     <p>Hai la carta fedeltà?
-    <input type="checkbox" name="Sconto" value=-10></p><br>
+    <input type="checkbox" name="Sconto" value=10></p><br>
     <input type="submit">
   </form>
 
@@ -30,22 +39,31 @@ La spedizione costa 10 euro, gli sconti si sovrappongono.
 
     if($_SERVER["REQUEST_METHOD"] == 'POST'){
       $Hogan = $_POST["Hogan"];
+      $NHogan = $_POST["NHogan"];
+
       $Nike = $_POST["Nike"];
-      $Sconto =  $_POST["Sconto"];
+      $NNike = $_POST["NNike"];
+
+      $Sconto =  $_POST["Sconto"]/100;
       $Prezzo = 0;
-      if(isset($_POST["Sconto"])){
-        $Prezzo += $Sconto;
-      }//Se selezionato sconto diminuisce il prezzo finale
 
       if(isset($_POST["Hogan"])){
-        $Prezzo += $Hogan;
+        $Prezzo += $Hogan * $NHogan;
       }//Se selezionato hogan viene aggiunto il prezzo delle Hogan a quello finale
 
       if(isset($_POST["Nike"])){
-        $Prezzo += $Nike;
+        $Prezzo += $Nike * $NNike;
       }//Se selezionato hogan viene aggiunto il prezzo delle Nike a quello finale
 
-      echo "Prezzo finale:  ". $Prezzo;
+      if($Prezzo < 100){
+        $Prezzo += 50;
+      }
+
+      if(isset($_POST["Sconto"])){
+        $Prezzo -= $Prezzo * $Sconto;
+      }//Se selezionato sconto diminuisce il prezzo finale
+
+      echo "Prezzo finale:  ". $Prezzo. "€";
     }
   ?>
 
